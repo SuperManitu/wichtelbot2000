779a198238b4a91f7d33932c6396b2f494fa91f5
Christoph Honal <christoph.honal@web.de>
2016-12-13 10:52:25 +0100

    Radical facelift

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var users = [];
 var presents = [];
 
-function addUser() {
+function addUser() 
+{
     var name = document.getElementById('name').value;
     var present = document.getElementById('geschenknummer').value;
     users.push(name);
@@ -11,26 +12,27 @@ function addUser() {
     console.log(users, presents);
 }
 
-function generateResult() {
-    var userCopy = users.map(function(u){ return u; })
-    var mapping = users.map(function(u){
-        var randomNumber = Math.floor(Math.random()*userCopy.length);
+function generateResult() 
+{
+    var userCopy = users.map(function(u) { return u; })
+    var mapping = users.map(function(u) {
+        var randomNumber = Math.floor(Math.random() * userCopy.length);
         var newUser = userCopy[randomNumber];
         userCopy.splice(randomNumber, 1);
         return newUser;
     });
 
-    var reorderedPresents = mapping.map(function(u){
+    var reorderedPresents = mapping.map(function(u) {
         return presents[users.indexOf(u)];
     });
 
-    var assignedPresents = reorderedPresents.map(function(_, i){
+    var assignedPresents = reorderedPresents.map(function(_, i) {
         return reorderedPresents[(i + 1) % reorderedPresents.length];
     });
 
-    var dict = mapping.map(function(u, i){
+    var dict = mapping.map(function(u, i) {
         return { [u]: assignedPresents[i] };
-    }).reduce(function(acc, curr){
+    }).reduce(function(acc, curr) {
         var tmp = {};
         for (var attrname in acc) { tmp[attrname] = acc[attrname]; }
         for (var attrname in curr) { tmp[attrname] = curr[attrname]; }
@@ -40,18 +42,18 @@ function generateResult() {
     displayResult(dict);
 }
 
-function displayResult(dict) {
-    while(document.body.firstChild) {
-        document.body.removeChild(document.body.firstChild);
-    }
+function displayResult(dict) 
+{
+    while(document.body.firstChild)  document.body.removeChild(document.body.firstChild);
 
-    var htmlString = "<table>"
+    var htmlString = "<div><table>"
         + "<tr>"
             + "<th>Name</th>"
-            + "<th>Geschenknummer</th>"
+            + "<th>Bekommt</th>"
         + "</tr>";
 
-    for(var name in dict) {
+    for(var name in dict) 
+    {
         htmlString +=
             "<tr>"
                 + "<td>" + name + "</td>"
@@ -59,7 +61,12 @@ function displayResult(dict) {
             + "</tr>";
     }
 
-    htmlString += "</table>";
-
+    htmlString += "</table></div><div class='action'><button type='button' onclick='location.reload()'>Nochmal</button></div>";
     document.body.insertAdjacentHTML("beforeend", htmlString);
 }
+
+/*
+var users = [ "Jan", "Phips", "Chris", "Lurch", "Fisch" ];
+var presents = [ 2, 4, 1, 5, 3 ];
+generateResult();
+*/
